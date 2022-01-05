e1dd496218f3a7d8df294c115e6d713255c35568
MseaC <96693341+MseaC@users.noreply.github.com>
2022-01-05 13:08:02 +0000

    Completed Module

--- ssg/site.py
+++ ssg/site.py
@@ -6,14 +6,11 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = ({self.dest, "/", path.relative_to(self.source)})
-        mkdir(directory, parents='True', exists_ok='True')
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
-        mkdir(self.dest, Parents='True', exists_ok='True')
-        for self.source.rglob("*"):
-            path = ({self.dest, "/", path.relative_to(self.source)})
-            if path.exists_ok is True
-                create_dir(path)
-
-
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
